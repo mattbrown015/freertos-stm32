5ab62ac1df695981938894c07efa61dee1375e0f
matthewb <matthewb@NB99.mshome.net>
2018-03-12 17:11:04 +0000

    Include the Discovery BSP and toggle the LEDs.
    Still not very exciting but the BSP makes it possible to check something
    really happened because the LEDs switch on.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
 #include <stm32l0xx_hal.h>
+#include <stm32l0538_discovery.h>
 
 int main()
 {
     HAL_Init();
+
+    BSP_LED_Init(LED3);
+    BSP_LED_Init(LED4);
+
+    BSP_LED_Toggle(LED3);
+    BSP_LED_Toggle(LED4);
 
     return 0;
 }
