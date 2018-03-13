66cc1b8848c3f67e7fc0dc5d951364e7f4a88924
mattbrown015 <matt_d_br@hotmail.com>
2018-03-13 11:31:30 +0000

    Added a second FreeRTOS task to flash the other LED.
    This was a bit tricky because I had to increase the heap size and I'm
    not sure about the significance of this except that the scheduler
    started. I read somewhere in the FreeRTOS instructions that the demo
    projects tend to be configured with the minimum heap for that project so
    hopefully it just means I copied the config from a single task demo.
    
    As I understand it, I've explicitly created 2 tasks but there is a
    third, the idle task, created by the FreeRTOS scheduler.

--- src/main.c
+++ src/main.c
@@ -17,6 +17,17 @@ static void toggleLed3Task()
     }
 }
 
+static void toggleLed4Task()
+{
+    for (;;)
+    {
+        BSP_LED_Toggle(LED4);
+
+        const TickType_t xDelay = 1000 / portTICK_PERIOD_MS;
+        vTaskDelay(xDelay);
+    }
+}
+
 int main()
 {
     HAL_Init();
@@ -24,8 +35,12 @@ int main()
     BSP_LED_Init(LED3);
     BSP_LED_Init(LED4);
 
-    const BaseType_t result = xTaskCreate(toggleLed3Task, "toggleLed3Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
-    if (result != pdPASS)
+    const BaseType_t result0 = xTaskCreate(toggleLed3Task, "toggleLed3Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+    if (result0 != pdPASS)
+        return EXIT_FAILURE;
+
+    const BaseType_t result1 = xTaskCreate(toggleLed4Task, "toggleLed4Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+    if (result1 != pdPASS)
         return EXIT_FAILURE;
 
     vTaskStartScheduler();
