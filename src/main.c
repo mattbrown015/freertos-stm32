23a2540187c60497541aadb97d5dceea0da56716
mattbrown015 <matt_d_br@hotmail.com>
2018-03-13 11:20:18 +0000

    Created FreeRTOS task to flash an LED.
    This is what I've been builing up to, a FreeRTOS task that does
    something! Not very exciting at the moment and there's still loads I
    don't understand but it's something.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,21 @@
+#include <FreeRTOS.h>
+#include <task.h>
+
 #include <stm32l0xx_hal.h>
 #include <stm32l0538_discovery.h>
+
+#include <stdlib.h>
+
+static void toggleLed3Task()
+{
+    for (;;)
+    {
+        BSP_LED_Toggle(LED3);
+
+        const TickType_t xDelay = 500 / portTICK_PERIOD_MS;
+        vTaskDelay(xDelay);
+    }
+}
 
 int main()
 {
@@ -8,8 +24,11 @@ int main()
     BSP_LED_Init(LED3);
     BSP_LED_Init(LED4);
 
-    BSP_LED_Toggle(LED3);
-    BSP_LED_Toggle(LED4);
+    const BaseType_t result = xTaskCreate(toggleLed3Task, "toggleLed3Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+    if (result != pdPASS)
+        return EXIT_FAILURE;
 
-    return 0;
+    vTaskStartScheduler();
+
+    return EXIT_FAILURE;
 }
