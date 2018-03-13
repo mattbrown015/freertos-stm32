17c47852212793a97daf4ee2a43817556d12da14
mattbrown015 <matt_d_br@hotmail.com>
2018-03-13 10:14:09 +0000

    Call the FreeRTOS tick handler from the STM32 Cube HAL tick handler.
    I couldn't find a clear description of this in the FreeRTOS
    instructions, I think it is Cortex-M0 specific. But, it is necessary.
    
    Also, I couldn't find a declaration of 'xPortSysTickHandler' in a
    library header. I guess this is also because it is Cortex-M0 specific.

--- src/stm32l0xx_it.c
+++ src/stm32l0xx_it.c
@@ -1,7 +1,11 @@
 #include "stm32l0xx_hal.h"
 
+extern void xPortSysTickHandler(void);
+
 void SysTick_Handler(void)
 {
     HAL_IncTick();
     HAL_SYSTICK_IRQHandler();
+
+    xPortSysTickHandler();
 }
