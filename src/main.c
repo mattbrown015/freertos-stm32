91ebddc518d20730203086e131e76b1441e6a1b5
mattbrown015 <matt_d_br@hotmail.com>
2018-03-14 11:59:10 +0000

    Configure the system clock explicitly.
    All the examples do something like this but I've been avoiding it
    because I don't understand it fully. I feel like I have to /bite the
    bullet/ now because I want to use the USART and the system clock is
    probably involved in getting the Baud rate correct.
    
    I copied this code from
    STM32Cube_FW_L0_V1.10.0\Projects\STM32L053C8-Discovery\Examples\UART\UART_TwoBoards_ComIT.
    
    I believe it sets the system clock to the maximum, 32 MHz when using the
    highest voltage. This is generated by the PLL driven by the HSI 16 MHz
    clock source. AFAICT the Discovery board doesn't include and external
    oscillator.
    
    The call to 'HAL_RCC_ClockConfig' is important because it updates
    'SystemCoreClock'. This surprised me slightly because the description of
    'SystemCoreClockUpdate' claims it must be called to update
    'SystemCoreClock'; AFAICT 'SystemCoreClockUpdate' isn't used.

--- src/main.c
+++ src/main.c
@@ -30,9 +30,63 @@ static void toggleLed4Task()
     }
 }
 
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSI)
+  *            SYSCLK(Hz)                     = 32000000
+  *            HCLK(Hz)                       = 32000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 1
+  *            APB2 Prescaler                 = 1
+  *            HSI Frequency(Hz)              = 16000000
+  *            PLL_MUL                        = 4
+  *            PLL_DIV                        = 2
+  *            Flash Latency(WS)              = 1
+  *            Main regulator output voltage  = Scale1 mode
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+
+    /* Enable Power Control clock */
+    __HAL_RCC_PWR_CLK_ENABLE();
+
+    /* The voltage scaling allows optimizing the power consumption when the device is 
+     clocked below the maximum system frequency, to update the voltage scaling value 
+     regarding system frequency refer to product datasheet. */
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    /* Enable HSI Oscillator and activate PLL with HSI as source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSEState = RCC_HSE_OFF;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL4;
+    RCC_OscInitStruct.PLL.PLLDIV = RCC_PLL_DIV2;
+    RCC_OscInitStruct.HSICalibrationValue = 0x10;
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+    /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1);
+}
+
 int main()
 {
     HAL_Init();
+
+    /* Configure the system clock to 32 MHz */
+    SystemClock_Config();
 
     BSP_LED_Init(LED3);
     BSP_LED_Init(LED4);
